a5fd05cf3e5739baabf32d4fe105219ae2f98ab6
Larissa Alves <1903larissaalves@gmail.com>
2021-01-27 13:39:23 -0300

    Continuando teste de integracao cadastrar turma

--- e2e/src/cadastrar-turma.e2e-spec.ts
+++ e2e/src/cadastrar-turma.e2e-spec.ts
@@ -11,5 +11,13 @@ describe("Testando tela cadastrar-turma", () => {
     it("deve ir para cadastrar turma", () => {
         cadastrarTurmaPage.acessarCadastrarTurmas()
     });
-    
+
+    it("deve cadastrar turma", () => {
+        expect(cadastrarTurmaPage.pegarInputNumero());
+        expect(cadastrarTurmaPage.pegarInputAno());
+        expect(cadastrarTurmaPage.pegarInputPeriodoLetivo());
+        expect(cadastrarTurmaPage.pegarInputVagas());
+        expect(cadastrarTurmaPage.pegarBotaoProximo().click());
+    })
+
 });
--- e2e/src/cadastrar-turma.po.ts
+++ e2e/src/cadastrar-turma.po.ts
@@ -1,9 +1,31 @@
-import { browser } from 'protractor'
+import { browser, by, element } from 'protractor'
 
 export class CadastrarTurmaPage{
 
     acessarCadastrarTurmas(){
         return browser.get('/stepper');
     }
+
+    pegarInputNumero(){
+        return element(by.css('input[formControlName="numero"]')).sendKeys('T-51');
+    }
+
+    pegarInputAno(){
+        return element(by.css('input[formControlName="ano"]')).sendKeys('2020');
+    }
+
+    pegarInputPeriodoLetivo(){
+        return element(by.css('input[formControlName="periodo"]')).sendKeys('2° Trimestre');
+    }
+
+    pegarInputVagas(){
+        return element(by.css('input[formControlName="vagas"]')).sendKeys(2);
+    }
+
+    pegarBotaoProximo(){
+        return element(by.buttonText('Próximo'));
+    }
+
+
 
 }
