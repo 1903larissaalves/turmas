9af0fb1ea656312f1a114e896f93b412ec387335
Larissa Alves <1903larissaalves@gmail.com>
2021-01-29 13:57:12 -0300

    Implemetado teste cadastros de turma

--- e2e/src/pageObject/turma/turma.e2e-spec.ts
+++ e2e/src/pageObject/turma/turma.e2e-spec.ts
@@ -9,15 +9,33 @@ describe("Testando tela cadastrar-turma", () => {
     });
 
     it("deve ir para cadastrar turma", () => {
-        cadastrarTurmaPage.acessarCadastrarTurmas()
+        cadastrarTurmaPage.getAcessarCadastrarTurmas()
     });
 
-    it("deve cadastrar turma", () => {
-        expect(cadastrarTurmaPage.pegarInput('numero', 'T-51'));
-        expect(cadastrarTurmaPage.pegarInput('ano', '2020'));
-        expect(cadastrarTurmaPage.pegarInput('periodo', '2° Trimestre'));
-        expect(cadastrarTurmaPage.pegarInput('vagas', '2'));
-        expect(cadastrarTurmaPage.pegarBotaoProximo().click());
+    it("deve cadastrar dados da turma", () => {
+        expect(cadastrarTurmaPage.getNumeroTurmaInput().sendKeys('T-203'));
+        expect(cadastrarTurmaPage.getAnoTurmaInput().sendKeys(2021));
+        expect(cadastrarTurmaPage.getPeriodoTurmaInput().sendKeys('1° Trimestre'));
+        expect(cadastrarTurmaPage.getVagasTurmaInput().sendKeys(2));
+        expect(cadastrarTurmaPage.getVagasTurmaInput().sendKeys(2));
+        expect(cadastrarTurmaPage.getProximoTurmaButton().click()); 
+    });
+
+    it("Deve cadastrar dados da nova disciplina e novo professor", () => {
+        expect(cadastrarTurmaPage.getNomeDisciplina().sendKeys('Logica programacao'));
+        expect(cadastrarTurmaPage.getProfessorDisciplina().sendKeys('David Junior'));
+        expect(cadastrarTurmaPage.getCargaHorariaDisciplina().sendKeys(40));
+        expect(cadastrarTurmaPage.getNomeProfessor().sendKeys('Ana Julia'));
+        expect(cadastrarTurmaPage.getTituloProfessor().sendKeys('PHD'));
+        expect(cadastrarTurmaPage.getAdiconarProfessorButton().click());
+    });
+
+    it("deve cadastrar dados de novo aluno", () => {
+        expect(cadastrarTurmaPage.getNomeAluno().sendKeys('Laura'));
+        expect(cadastrarTurmaPage.getIngressoAluno().sendKeys('Enade'));
+        expect(cadastrarTurmaPage.getVoltarButton().click());
+        expect(cadastrarTurmaPage.getAdcionarAlunoButton().click());
+        expect(cadastrarTurmaPage.getConcluirTurmaButton().click());
     });
 
 });
--- e2e/src/pageObject/turma/turma.po.ts
+++ e2e/src/pageObject/turma/turma.po.ts
@@ -1,17 +1,79 @@
-import { browser, by, element } from 'protractor'
+import { $, browser, by, element, ElementFinder } from 'protractor'
 
 export class TurmaPage{
 
-    acessarCadastrarTurmas(){
+    getAcessarCadastrarTurmas(){
         return browser.get('/stepper');
     }
 
-    pegarInput(formControlName: any, valor: any){
-        return element(by.css(`input[formControlName="${formControlName}"]`)).sendKeys(valor);
+    getNumeroTurmaInput(): ElementFinder{
+        return $('#numeroTurma input');
     }
 
-    pegarBotaoProximo(){
-        return element(by.buttonText('Próximo'));
+    getAnoTurmaInput(): ElementFinder{
+        return $('#anoTurma input');
+    }
+
+    getPeriodoTurmaInput(): ElementFinder{
+        return $('#periodoTurma input');
+    }
+
+    getVagasTurmaInput(): ElementFinder{
+        return $('#vagasTurmas input');
+    }
+
+    getProximoTurmaButton(): ElementFinder{
+        return $('#ProximoTurmaBtn button');
+    }
+
+    /*Disciplina*/
+
+    getNomeDisciplina(): ElementFinder{
+       return $('#nomeDisciplina input'); 
+    }
+    
+    getProfessorDisciplina(): ElementFinder{
+        return $('#ProfessorDisciplina input'); 
+    }
+    
+    getCargaHorariaDisciplina(): ElementFinder{
+        return $('#cargaHorariaDisciplina input'); 
+    }
+
+    getAdiconarProfessorButton(): ElementFinder{
+        return $('#adicionarProfessorDisciplinaBtn button'); 
+    }
+
+    /*Professor*/
+
+    getNomeProfessor(): ElementFinder{
+        return $('#nomeProfessor input'); 
+    }
+
+    getTituloProfessor(): ElementFinder{
+        return $('#tituloProfessor input'); 
+    }   
+
+    /*Aluno*/
+
+    getNomeAluno(): ElementFinder{
+        return $('#nomeAluno input')
+    }
+
+    getIngressoAluno(): ElementFinder{
+        return $('#ingressoAluno input')
+    }
+
+    getVoltarButton(): ElementFinder{
+        return $('#voltarBtn button')
+    }
+
+    getAdcionarAlunoButton(): ElementFinder{
+        return $('#AdicionarAlunoBtn button')
+    }
+
+    getConcluirTurmaButton(): ElementFinder{
+        return $('#conluirTurmaBtn button')
     }
 
 }
